f051bf17f81651dc0d3dae369a0418cc2ea88f96
Odeneho-Calculus <calculus069@gmail.com>
2025-12-13 11:34:34 +0000

    optimized andcreated new components v6

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.get('/health', (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
